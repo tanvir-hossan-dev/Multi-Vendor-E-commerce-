41a5c3befbbe8d60c0011e5e88196ef1bfbded7f
Tanvir Hossan <tanvirhossan27@gmail.com>
2022-12-28 10:29:04 +0600

    Chang: Code changed in Authenticate and product file

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-const Authenticate = require("../middleware/Authenticate");
 const userRouter = require("./auth");
 const productRouter = require("./product");
 
 router.use("/api/v1/auth", userRouter);
-router.use("/api/v1/product", Authenticate, productRouter);
+router.use("/api/v1/product", productRouter);
 
 module.exports = router;
--- server/routers/product.js
+++ server/routers/product.js
@@ -1,10 +1,11 @@
 const { addProduct, updateProduct, deleteProduct, getProduct } = require("../controllers/Product");
+const Authenticate = require("../middleware/Authenticate");
 
 const router = require("express").Router();
 
-router.put("/updateproduct/:id", updateProduct);
-router.delete("/deleteproduct/:id", deleteProduct);
-router.post("/addproduct", addProduct);
+router.put("/updateproduct/:id", Authenticate, updateProduct);
+router.delete("/deleteproduct/:id", Authenticate, deleteProduct);
+router.post("/addproduct", Authenticate, addProduct);
 router.get("/getproduct", getProduct);
 
 module.exports = router;
